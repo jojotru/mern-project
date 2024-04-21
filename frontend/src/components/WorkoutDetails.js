5649d92e6f1e34e343afb0260fda97db5cba318e
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-04-21 00:10:50 -0400

    Update time log and finishing touches.

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,6 +1,5 @@
 import {useWorkoutsContext} from '../hooks/useWorkoutsContext'
 
-//date fns
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({workout}) => {
@@ -22,7 +21,7 @@ const WorkoutDetails = ({workout}) => {
             <h4>{workout.title}</h4>
             <p><strong>Count: </strong>{workout.count}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
-            <p>{formatDistanceToNow(new Date(workout.createAt), {addSuffix: true})}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
             <span className = "material-symbols-outlined" onClick = {handleClick}>delete</span>
         </div>
     )
